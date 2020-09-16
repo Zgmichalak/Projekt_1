eeda6b30b62816962984bb888affc1dcd86a68eb
zg.michalak <zg.michalak@gmail.com>
2020-09-16 17:54:01 +0200

    Add IF instruction

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const names = [];
+const div = document.querySelector('div');
 
 const addName = (e) => {
     e.preventDefault();
     const input = document.querySelector('input');
     const name = input.value;
-    names.push(name);
-    
+    if() {
+        names.push(name);
+        div.textContent += name;
+    }
 }
 
 document.querySelector('button').addEventListener('click', addName);
